add97802be9939aafd0f7e20147031c890c687ec
mehul <mehulsolanki.dkwebworld@gmail.com>
2018-06-13 14:27:08 +0530

    Removed unused code also format code that required.

--- app/src/main/java/com/m/nfc_rfid/MainActivity.java
+++ app/src/main/java/com/m/nfc_rfid/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         textViewInfo = (TextView) findViewById(R.id.info);
         textViewTagInfo = (TextView) findViewById(R.id.taginfo);
-        textViewBlock = (TextView)findViewById(R.id.block);
+        textViewBlock = (TextView) findViewById(R.id.block);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
@@ -46,82 +46,40 @@ public class MainActivity extends AppCompatActivity {
         String action = intent.getAction();
 
         if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-            Toast.makeText(this,
-                    "onResume() - ACTION_TECH_DISCOVERED",
-                    Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "onResume() - ACTION_TECH_DISCOVERED", Toast.LENGTH_SHORT).show();
 
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            if(tag == null){
+            if (tag == null) {
                 textViewInfo.setText("tag == null");
-            }else{
+            } else {
                 String tagInfo = tag.toString() + "\n";
 
                 tagInfo += "\nTag Id: \n";
                 byte[] tagId = tag.getId();
-                tagInfo += "length = " + tagId.length +"\n";
-                for(int i=0; i<tagId.length; i++){
+                tagInfo += "length = " + tagId.length + "\n";
+                for (int i = 0; i < tagId.length; i++) {
                     tagInfo += String.format("%02X", tagId[i] & 0xff) + " ";
                 }
                 tagInfo += "\n";
 
                 String[] techList = tag.getTechList();
                 tagInfo += "\nTech List\n";
-                tagInfo += "length = " + techList.length +"\n";
-                for(int i=0; i<techList.length; i++){
+                tagInfo += "length = " + techList.length + "\n";
+                for (int i = 0; i < techList.length; i++) {
                     tagInfo += techList[i] + "\n ";
                 }
 
                 textViewInfo.setText(tagInfo);
-
                 //Only android.nfc.tech.MifareClassic specified in nfc_tech_filter.xml,
                 //so must be MifareClassic
                 readMifareClassic(tag);
             }
-        }else{
-            Toast.makeText(this,
-                    "onResume() : " + action,
-                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "onResume() : " + action, Toast.LENGTH_SHORT).show();
         }
-
-        //Intent intent = getIntent();
-        //String action = intent.getAction();
-
-        /*if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
-            Toast.makeText(this,
-                    "onResume() - ACTION_TAG_DISCOVERED",
-                    Toast.LENGTH_SHORT).show();
-
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            if(tag == null){
-                textViewInfo.setText("tag == null");
-            }else{
-                String tagInfo = tag.toString() + "\n";
-
-                tagInfo += "\nTag Id: \n";
-                byte[] tagId = tag.getId();
-                tagInfo += "length = " + tagId.length +"\n";
-                for(int i=0; i<tagId.length; i++){
-                    tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
-                }
-                tagInfo += "\n";
-
-                String[] techList = tag.getTechList();
-                tagInfo += "\nTech List\n";
-                tagInfo += "length = " + techList.length +"\n";
-                for(int i=0; i<techList.length; i++){
-                    tagInfo += techList[i] + "\n ";
-                }
-
-                textViewInfo.setText(tagInfo);
-            }
-        }else{
-            Toast.makeText(this,
-                    "onResume() : " + action,
-                    Toast.LENGTH_SHORT).show();
-        }*/
     }
 
-    /*public void readMifareClassic(Tag tag) {
+    public void readMifareClassic(Tag tag) {
         MifareClassic mifareClassicTag = MifareClassic.get(tag);
 
         String typeInfoString = "--- MifareClassic tag ---\n";
@@ -167,66 +125,14 @@ public class MainActivity extends AppCompatActivity {
         typeInfoString += "SectorCount \t= " + sectorCount + "\n";
 
         textViewTagInfo.setText(typeInfoString);
-    }*/
-
-    public void readMifareClassic(Tag tag){
-        MifareClassic mifareClassicTag = MifareClassic.get(tag);
-
-        String typeInfoString = "--- MifareClassic tag ---\n";
-        int type = mifareClassicTag.getType();
-        switch(type){
-            case MifareClassic.TYPE_PLUS:
-                typeInfoString += "MifareClassic.TYPE_PLUS\n";
-                break;
-            case MifareClassic.TYPE_PRO:
-                typeInfoString += "MifareClassic.TYPE_PRO\n";
-                break;
-            case MifareClassic.TYPE_CLASSIC:
-                typeInfoString += "MifareClassic.TYPE_CLASSIC\n";
-                break;
-            case MifareClassic.TYPE_UNKNOWN:
-                typeInfoString += "MifareClassic.TYPE_UNKNOWN\n";
-                break;
-            default:
-                typeInfoString += "unknown...!\n";
-        }
-
-        int size = mifareClassicTag.getSize();
-        switch(size){
-            case MifareClassic.SIZE_1K:
-                typeInfoString += "MifareClassic.SIZE_1K\n";
-                break;
-            case MifareClassic.SIZE_2K:
-                typeInfoString += "MifareClassic.SIZE_2K\n";
-                break;
-            case MifareClassic.SIZE_4K:
-                typeInfoString += "MifareClassic.SIZE_4K\n";
-                break;
-            case MifareClassic.SIZE_MINI:
-                typeInfoString += "MifareClassic.SIZE_MINI\n";
-                break;
-            default:
-                typeInfoString += "unknown size...!\n";
-        }
-
-        int blockCount = mifareClassicTag.getBlockCount();
-        typeInfoString += "BlockCount \t= " + blockCount + "\n";
-        int sectorCount = mifareClassicTag.getSectorCount();
-        typeInfoString += "SectorCount \t= " + sectorCount + "\n";
-
-        textViewTagInfo.setText(typeInfoString);
-
         new ReadMifareClassicTask(mifareClassicTag).execute();
 
     }
 
     private class ReadMifareClassicTask extends AsyncTask<Void, Void, Void> {
-
-        /*
-        MIFARE Classic tags are divided into sectors, and each sector is sub-divided into blocks.
+        /* MIFARE Classic tags are divided into sectors, and each sector is sub-divided into blocks.
         Block size is always 16 bytes (BLOCK_SIZE). Sector size varies.
-        MIFARE Classic 1k are 1024 bytes (SIZE_1K), with 16 sectors each of 4 blocks.
-        */
+        MIFARE Classic 1k are 1024 bytes (SIZE_1K), with 16 sectors each of 4 blocks. */
 
         MifareClassic taskTag;
         int numOfBlock;
@@ -236,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         final int numOfBlockInSector = 4;
         byte[][][] buffer = new byte[numOfSector][numOfBlockInSector][MifareClassic.BLOCK_SIZE];
 
-        ReadMifareClassicTask(MifareClassic tag){
+        ReadMifareClassicTask(MifareClassic tag) {
             taskTag = tag;
             success = false;
         }
@@ -249,24 +155,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... params) {
-
             try {
                 taskTag.connect();
-
-                for(int s=0; s<numOfSector; s++){
-                    if(taskTag.authenticateSectorWithKeyA(s, MifareClassic.KEY_DEFAULT)) {
-                        for(int b=0; b<numOfBlockInSector; b++){
+                for (int s = 0; s < numOfSector; s++) {
+                    if (taskTag.authenticateSectorWithKeyA(s, MifareClassic.KEY_DEFAULT)) {
+                        for (int b = 0; b < numOfBlockInSector; b++) {
                             int blockIndex = (s * numOfBlockInSector) + b;
                             buffer[s][b] = taskTag.readBlock(blockIndex);
                         }
                     }
                 }
-
                 success = true;
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally{
-                if(taskTag!=null){
+            } finally {
+                if (taskTag != null) {
                     try {
                         taskTag.close();
                     } catch (IOException e) {
@@ -274,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-
             return null;
         }
 
@@ -282,12 +184,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             //display block
-            if(success){
+            if (success) {
                 String stringBlock = "";
-                for(int i=0; i<numOfSector; i++){
+                for (int i = 0; i < numOfSector; i++) {
                     stringBlock += i + " :\n";
-                    for(int j=0; j<numOfBlockInSector; j++){
-                        for(int k=0; k<MifareClassic.BLOCK_SIZE; k++){
+                    for (int j = 0; j < numOfBlockInSector; j++) {
+                        for (int k = 0; k < MifareClassic.BLOCK_SIZE; k++) {
                             stringBlock += String.format("%02X", buffer[i][j][k] & 0xff) + " ";
                         }
                         stringBlock += "\n";
@@ -295,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
                     stringBlock += "\n";
                 }
                 textViewBlock.setText(stringBlock);
-            }else{
+            } else {
                 textViewBlock.setText("Fail to read Blocks!!!");
             }
         }
